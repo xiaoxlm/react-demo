d6178e6e4984f5b88da5ff12465454ee6b624d81
xiaoxlm <332192810@qq.com>
2021-02-09 16:22:37 +0800

    user 初步完成

--- src/containers/User/index.js
+++ src/containers/User/index.js
@@ -1,13 +1,24 @@
-import { Component } from "react"
+import React, { Component } from 'react';
+import UserMain from "./components/UserMain"
+import UserHeader from "./components/UserHeader" // 1. 引入
 
-class User extends Component  {
+class User extends Component {
     render() {
         return (
             <div>
-
+                <UserHeader onBack={this.handleBack} onLogout={this.handleLogout}/>
+                <UserMain/>
             </div>
-        )
+        );
+    }
+
+    handleBack = () => {
+        // todo
+    }
+
+    handleLogout = () => {
+        // todo
     }
 }
 
-export default User
+export default User;
